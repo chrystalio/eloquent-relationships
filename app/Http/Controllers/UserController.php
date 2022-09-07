ba6ed71732ef0bdfefc5f16560dede3f711464a5
Kristoff <chrystalio.k@outlook.com>
2022-09-07 16:14:35 +0700

    Add UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // Get users from model
+        $users = User::latest()->get();
 
+        // Return view with users
+        return view('users', compact('users'));
     }
 }
